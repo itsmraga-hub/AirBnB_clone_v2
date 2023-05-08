d4853c0de0c87a16885d74870939d18f010042e1
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-08 14:19:31 +0300

    Task 0: correct

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,9 +9,7 @@ sudo apt install -y nginx
 sudo ufw allow 'Nginx HTTP'
 
 #sudo systemctl restart nginx
-sudo service nginx start
 
-sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
 sudo echo "Hello World, Release Test Index.html" > /data/web_static/releases/test/index.html
@@ -21,14 +19,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
 
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-sudo sed -i '/server {/a location /hbnb_static {\nalias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
-
-
-sudo nginx -t
-
-if [ $? -eq 0 ]; then
-	sudo systemctl reload nginx
-else
-	exit 1
-fi
+sudo service nginx start
