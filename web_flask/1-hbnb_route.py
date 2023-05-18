6a1e4e29c023cf33d95aa14999d1e3e3099a854d
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-18 18:29:37 +0300

    Task 1: hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    A script that starts a Flask web application
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return f'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+# Start the Flask application
+if __name__ == '__main__':
+    app.run()
