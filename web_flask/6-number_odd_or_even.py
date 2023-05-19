260c0dddc018ca71599447b2d191077f7db6ea7d
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-19 20:20:45 +0300

    Task 6: correction

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -108,7 +108,6 @@ def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n)
 
 
-
 # Start the Flask application
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
