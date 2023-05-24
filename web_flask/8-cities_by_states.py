34134c2783c8d19fda049d6844401d9dd728048c
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-24 22:56:22 +0300

    Task 8: cities by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application and fetches states from the database
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """
+        Remove current SQLAlchemy Session after each request
+    """
+    storage.close()
+
+@app.route('/cities_by_state', strict_slashes=False)
+def cities_by_state():
+    """
+        H1 tag: “States”
+        UL tag: with the list of all State objects present in
+        DBStorage sorted by name (A->Z)
+    """
+    states = storage.all(State).values()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
